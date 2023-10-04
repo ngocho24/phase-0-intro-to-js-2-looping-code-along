23ac5e9f7f6fd2f0482757408a92fff4ef7372cf
Elijah Ngocho <elijahngocho24@gmail.com>
2023-10-04 08:57:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// message for each array
+const birthDay = ["Johan", "Joy", "Njoki"];
+
+for ( let i = 0; i < birthDay.length; i++){
+console.log ("Thank you, " + birthDay[i])
+};
+
+// decremental loop using while
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  countDown(10);
+  
